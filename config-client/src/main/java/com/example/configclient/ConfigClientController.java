bab25dc27876ed23ae93ec882a2e02e6f1b7e519
939514820 <939514820@qq.com>
2021-01-13 21:08:28 +0800

    test

--- config-client/src/main/java/com/example/configclient/ConfigClientController.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientController.java
@@ -2,18 +2,27 @@ package com.example.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
 
-    @Value("${cs.name}")
-    String name;
-    @Value("${cs.age}")
-    String age;
+//    @Value("${cs.name}")
+//    String name;
+//    @Value("${cs.age}")
+//    String age;
+//
+//    @GetMapping(value = "/hi")
+//    public String hi(){
+//        return "我的名字是:"+name+",年龄是:"+age;
+//    }
 
-    @GetMapping(value = "/hi")
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
     public String hi(){
-        return "我的名字是:"+name+",年龄是:"+age;
+        return foo;
     }
 }
