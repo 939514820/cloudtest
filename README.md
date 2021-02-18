74f49cdf09d4d293bc26a785e1de74384b43f8ae
939514820 <939514820@qq.com>
2021-02-18 16:57:22 +0800

    todo list

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cloudtest
+1.服务发现:Euraka <br/>
+2.服务配置:config  <br/>
+3.网关:gateway,zuul  <br/>
+4.服务保护:hytrix,sentinel  <br/>
+5.服务监控:skywalking,zipkin <br/>
+6.线上问题排查
---
+++ service/src/main/java/com/example/service/hytrix/HytrixTestController.java
@@ -0,0 +1,23 @@
+package com.example.service.hytrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HytrixTestController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+}
